caa93c1ff72a0f538dedb6fc7a777ea4223d1b0f
David Yunker <david.yunker@gmail.com>
2016-08-09 14:39:07 -0500

    got objects displaying in the console

--- quiz.js
+++ quiz.js
@@ -1,9 +1,38 @@
 $(document).ready(function() {
 
+//first try
+  // $.getJSON("categories.json")
+  //   .then(function(categories) {
+  //     $.getJSON("types.json")
+  // })
+  //   .then(function(types){
+  //     $.getJSON("products.json")
+  //   })
+  //   .then(function(products) {
+  //     console.log(categories)
+  //     console.log(types)
+  //     console.log(products)
+  //   })
 
 
+// if you need both sets of data you will need to cache the first dataset
 
+var _catList // create a state variable to hold the data
+var _typeList
+$.getJSON('categories.json')
+  .then(function (catList) {
+    _catList = catList // cache the data
+    return $.getJSON('types.json')
+  })
+  .then(function (typeList) {
+    _typeList = typeList
+    return $.getJSON('products.json')
+  })
+  .then(function (productsList){
+    console.log(productsList)
+    console.log(_typeList)
+    console.log(_catList)
 
-
+  })
 
 })
